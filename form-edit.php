366f0c5bc54c85ac91e6c94b61556da274786b47
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-12-24 12:58:27 +0700

    form Pendaftaran siswa baru

--- form-edit.php
+++ form-edit.php
@@ -48,8 +48,8 @@ if(mysqli_num_rows($query) < 1) {
         <p>
             <label for="jenis_kelamin">Jenis Kelamin</label>   
             <?php $jk = $siswa['jenis_kelamin']; ?>
-            <label><input type="radio" name="jenis_kelamin" value="laki-laki" <?php echo ($jk == 'laki-laki') ? "checked": "" ?>> Laki-laki</label>
-            <label><input type="radio" name="jenis_kelamin" value="perempuan" <?php echo ($jk == 'perempuan') ? "checked": "" ?>> Perempuan</label>
+            <label><input type="radio" name="jenis_kelamin" value="Laki-laki" <?php echo ($jk == 'laki-laki') ? "checked": "" ?>> Laki-laki</label>
+            <label><input type="radio" name="jenis_kelamin" value="Perempuan" <?php echo ($jk == 'perempuan') ? "checked": "" ?>> Perempuan</label>
 </p>
 <p>
     <label for="agama">Agama</label>
