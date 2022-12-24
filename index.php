ae9ef1d1c2ca61812135697e0ba7c7907ec3309b
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-12-24 09:53:17 +0700

    form Pendaftaran siswa baru

--- index.php
+++ index.php
@@ -20,5 +20,16 @@
             <li><a href="list-siswa.php">Pendaftar<h2>
         </ul>
     </nav>
+    <?php if(isset($_GET['status'])): ?>
+        <p>
+            <?php
+            if($_GET['status'] == 'sukses') {
+                echo "Pendaftaran siswa baru berhasil!!!";
+            } else {
+                echo "Pendaftaran gagal!";
+            }
+            ?>
+        </p>
+        <?php endif; ?>
 </body>
 </html>
